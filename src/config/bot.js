8437c47291610b1e0f45082dd879af9d3a8603ce
ayamemikushu96 <ayamemikushu96@gmail.com>
2026-05-17 00:24:46 -0400

    Update bot primary and secondary colors

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#523e61", 
-      secondary: "#bfe7e7", 
+      primary: "#336699", 
+      secondary: "#2f3136", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
